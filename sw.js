ffddeec86f6c9e437f8132f473a63bff33f7a3e0
zz413012000 <zz413012000@gmail.com>
2022-11-03 09:35:33 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-abe82c4be56300e678b4.js"
+    "url": "webpack-runtime-b81c3a80caa796e08706.js"
   },
   {
     "url": "framework-4704efbcb7f53137667f.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "abcb40a9efb4d701aa783c3709a22124"
+    "revision": "b371f0b6741315c6de0e19e51ba79261"
   },
   {
     "url": "polyfill-93c7ef2bd9362f5d2424.js"
